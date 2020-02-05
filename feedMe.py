ebae2d21f0acbb98a63456792cc13a41a2b496f4
NoahMalhi <malhin@bu.edu>
2020-02-04 20:25:02 -0500

    Integrated tweepy with google vision. Baseline complete and working

--- feedMe.py
+++ feedMe.py
@@ -1,5 +1,10 @@
 import tweepy
 import keys
+import io
+import os
+import wget
+from google.cloud import vision
+from google.cloud.vision import types
 
 auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
 auth.set_access_token(keys.access_token, keys.access_token_secret)
@@ -9,3 +14,29 @@ api = tweepy.API(auth)
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print(tweet.text)
+
+    media_files = set()
+for status in public_tweets:
+    media = status.entities.get('media', [])
+    if(len(media) > 0):
+        media_files.add(media[0]['media_url'])
+
+for media_file in media_files:
+    test = wget.download(media_file)
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+    # The name of the image file to annotate
+    file_name = os.path.abspath(test)
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+    image = types.Image(content=content)
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    print('')
+    print('Description ' + test + ':')
+    for label in labels:
+        print(label.description)
+
+
--- image_test.py
+++ image_test.py
@@ -9,14 +9,13 @@ from google.cloud.vision import types
 client = vision.ImageAnnotatorClient()
 
 # The name of the image file to annotate
-file_name = os.path.abspath('resources/wakeupcat.jpg')
+file_name = os.path.abspath('/home/nomal/Pictures/dawoud.jpeg')
 
 # Loads the image into memory
 with io.open(file_name, 'rb') as image_file:
     content = image_file.read()
 
 image = types.Image(content=content)
-
 # Performs label detection on the image file
 response = client.label_detection(image=image)
 labels = response.label_annotations
