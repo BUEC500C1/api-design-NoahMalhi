323f07e6cebc81dcb704d6517910f0cae9567e0b
NoahMalhi <malhin@bu.edu>
2020-02-05 17:49:28 -0500

    Added testing

--- feedMe.py
+++ feedMe.py
@@ -6,37 +6,46 @@ import wget
 from google.cloud import vision
 from google.cloud.vision import types
 
-auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
-auth.set_access_token(keys.access_token, keys.access_token_secret)
+def vision_feed():
+    try:
+        auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
+        auth.set_access_token(keys.access_token, keys.access_token_secret)
 
-api = tweepy.API(auth)
+        api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+        public_tweets = api.home_timeline()
+        for tweet in public_tweets:
+            print(tweet.text)
 
-    media_files = set()
-for status in public_tweets:
-    media = status.entities.get('media', [])
-    if(len(media) > 0):
-        media_files.add(media[0]['media_url'])
+            media_files = set()
 
-for media_file in media_files:
-    test = wget.download(media_file)
-    # Instantiates a client
-    client = vision.ImageAnnotatorClient()
-    # The name of the image file to annotate
-    file_name = os.path.abspath(test)
-    # Loads the image into memory
-    with io.open(file_name, 'rb') as image_file:
-        content = image_file.read()
-    image = types.Image(content=content)
-    # Performs label detection on the image file
-    response = client.label_detection(image=image)
-    labels = response.label_annotations
-    print('')
-    print('Description ' + test + ':')
-    for label in labels:
-        print(label.description)
+        #as shown on google tutorial
+        for status in public_tweets:
+            media = status.entities.get('media', [])
+            if(len(media) > 0):
+                media_files.add(media[0]['media_url'])
 
+        for media_file in media_files:
+            test = wget.download(media_file)
+            # Instantiates a client
+            client = vision.ImageAnnotatorClient()
+            # The name of the image file to annotate
+            file_name = os.path.abspath(test)
+            # Loads the image into memory
+            with io.open(file_name, 'rb') as image_file:
+                content = image_file.read()
+            image = types.Image(content=content)
+            # Performs label detection on the image file
+            response = client.label_detection(image=image)
+            labels = response.label_annotations
+            print('')
+            print('Description ' + test + ':')
+            for label in labels:
+                print(label.description)
 
+            success = 1
+
+    except ValueError:
+        success = 0
+
+vision_feed()
---
+++ test_feedMe.py
@@ -0,0 +1,6 @@
+import feedMe
+import pytest
+
+#just tests successfull run
+def vision_feed():
+    assert success = 1
